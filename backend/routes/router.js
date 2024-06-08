fbdd2eb1d9e7357e8ff2521ac3b679de630500c5
Tigran Bantikyan <tigranbantikyan@gmail.com>
2024-06-08 17:28:19 -0500

    handle options requests

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -9,6 +9,13 @@ const UserModel = require("./models/UserModel")
 const router = express.Router();
 dotenv.config(); // use .env
 
+router.options("/*", function(req, res, next){
+    res.header('Access-Control-Allow-Origin', 'https://tachy.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  });
+
 // get token from header and verify authorization
 isAuthorized = (req, res, next) => {
     const authHeader = req.headers.authorization;
