cfe13dc0f6207098a2cb35c156cc86a5da6ff9de
Tigran Bantikyan <tigranbantikyan@gmail.com>
2024-06-09 21:02:20 -0400

    debugging

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -39,7 +39,7 @@ export default function LoginPage() {
                     })
                     toast.error(resp.error)
                 } else { // login succeeded
-                    console.log("Cookies ", () => cookies.getAll())
+                    console.log("Cookies ", cookies.getAll())
                     setToken(cookies.get("token"))
                     setAuth(true)
                     toast.success("Login successful")
