4de1fd56f5d5b205d0057ec53448b073e6e6c751
Tigran Bantikyan <tigranbantikyan@gmail.com>
2024-06-09 21:39:01 -0400

    updated token logic

--- frontend/src/api/loginRequest.js
+++ frontend/src/api/loginRequest.js
@@ -3,7 +3,6 @@ import { API_URL } from "./config"
 export default (user) => {
   return fetch(`${API_URL}/login`, {
     method: 'POST',
-    // credentials: "include",
     headers: {
       "Content-Type": 'application/json',
     },
